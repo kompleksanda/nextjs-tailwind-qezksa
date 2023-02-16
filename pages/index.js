235a0bb46cf8dfaf03950aadb55e496bd4bca378
kompleksanda <46966054+kompleksanda@users.noreply.github.com>
2023-02-16 18:46:33 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,13 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
-      <div className="bg-slate-100 h-screen w-full"></div>
+      <div className="bg-slate-100 h-screen w-full">
+        <div className="flex bg-white p-2 justify-between">
+          <div className="bg-slate-100 h-12 w-12"></div>
+          <div className="flex bg-slate-100 h-12 w-12"></div>
+          <div className="flex bg-slate-100 h-12 w-12"></div>
+        </div>
+      </div>
     </>
   );
 }
