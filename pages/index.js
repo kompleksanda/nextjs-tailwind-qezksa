2ef86207ba4da396aa235257d9f5fbe7a548c615
kompleksanda <46966054+kompleksanda@users.noreply.github.com>
2023-02-18 14:25:28 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,11 @@ export default function Home() {
       <div className="bg-slate-100 h-screen w-full">
         <div className="flex bg-white p-2 justify-between">
           <div className="bg-slate-100 h-12 w-12"></div>
-          <div className="flex bg-slate-100 h-12 w-12"></div>
-          <div className="flex bg-slate-100 h-12 w-12"></div>
+          <div className="flex gap-x-2">
+            <div className="h-12 w-12 bg-slate-300"></div>
+            <div className="h-12 w-12 bg-slate-300"></div>
+          </div>
+          <div className="bg-slate-100 h-12 w-12"></div>
         </div>
       </div>
     </>
