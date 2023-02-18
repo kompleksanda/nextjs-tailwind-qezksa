20f2fe4d0da9849379ad82d48caf20abdcaa9ff9
kompleksanda <46966054+kompleksanda@users.noreply.github.com>
2023-02-18 17:27:25 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-
+const listt = ['men', 'women', 'child'];
 export default function Home() {
   return (
     <>
@@ -7,13 +7,10 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <div className="bg-slate-100 h-screen w-full">
-        <div className="flex bg-white p-2 justify-between">
-          <div className="bg-slate-100 h-12 w-12"></div>
-          <div className="flex gap-x-2">
-            <div className="h-12 w-12 bg-slate-300"></div>
-            <div className="h-12 w-12 bg-slate-300"></div>
-          </div>
-          <div className="bg-slate-100 h-12 w-12"></div>
+        <div className="flex bg-white py-6 gap-x-3 justify-center">
+          {listt.map((item, i) => {
+            <div>{item}</div>;
+          })}
         </div>
       </div>
     </>
