7bd61b5cbf4edd34c52a2a9301be4382a4a95120
kompleksanda <46966054+kompleksanda@users.noreply.github.com>
2023-02-19 10:30:06 +0100

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-const listt = ['men', 'women', 'child'];
 export default function Home() {
   return (
     <>
@@ -8,9 +7,9 @@ export default function Home() {
       </Head>
       <div className="bg-slate-100 h-screen w-full">
         <div className="flex bg-white py-6 gap-x-3 justify-center">
-          {listt.map((item, i) => {
-            <div>{item}</div>;
-          })}
+          {['men', 'women', 'child'].map((item, i) => (
+            <div>{item}</div>
+          ))}
         </div>
       </div>
     </>
