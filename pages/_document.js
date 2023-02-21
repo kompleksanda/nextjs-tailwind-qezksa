01108409c57373c6e4868b6e951adcdc1f73268e
kompleksanda <46966054+kompleksanda@users.noreply.github.com>
2023-02-21 17:22:20 +0100

    1 file modified, 1 added

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="bg-red-500 container mx-auto pt-4">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,11 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
-      <section class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat h-screen w-screen">
-        <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"></div>
+      <section class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+        <div className="absolute bg-white/50 inset-0"></div>
+        {[...Array(20)].map((_, i) => (
+          <div>{i}</div>
+        ))}
       </section>
     </>
   );
