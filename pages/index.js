809720409914dc8c3d6162217f54fe3c2d39e8f5
Alexander Akinyomola <bynarikompleks@gmail.com>
2023-02-22 17:12:45 +0000

    Large commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <time className="flex max-w-sm bg-white items-center gap-2 shadow-sm px-2">
           <span>2022</span>
           <span className="h-px bg-black flex-1"></span>
-          <span>Oct 10</span>
+          <span>Oct 1010</span>
         </time>
       </div>
     </>
